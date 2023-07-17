d20ba9a01ffdfb82a6ad4225de2dcf9433d4d9ac
Damir Kabirov <44746689+Damir-Kabirov@users.noreply.github.com>
2023-07-17 21:57:28 +0300

    setting webpack and division module

---
+++ src/components/btn-load-more.js
@@ -0,0 +1,3 @@
+export const btnLoadMoreTemplate = ()=> {
+  return `<button class="load-more" type="button">load more</button>`;
+};
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`,
+  entry: `./src/main.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devtool: `source-map`,
+  devServer: {
+    static: path.join(__dirname, `public`),
+    port:8080,
+    compress: true,
+    hot: true
+  }
+};
